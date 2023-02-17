b2c84eb1afcdf020bfcb739730e1d03da51c228a
Marcell Biemann <mbiemann@gmail.com>
2023-02-17 15:03:33 +0000

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -2,4 +2,4 @@ from boto3 import client
 
 
 def before_all(context):
-    context.api_url = client("ssm").get_parameter(Name=f"/briefbox/outputs/briefbox/api-url")["Parameter"]["Value"]
+    context.api_url = client("ssm").get_parameter(Name=f"/briefbox/outputs/briefbox-backend/api-url")["Parameter"]["Value"]
